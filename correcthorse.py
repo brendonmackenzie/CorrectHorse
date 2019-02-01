64ee7072110ec28c95dd2031d3fcb07db88fcdad
Brendon <brendon@primitiveman.co.nz>
2019-02-01 23:01:39 +1300

    converted tabs to spaces

--- correcthorse.py
+++ correcthorse.py
@@ -6,17 +6,17 @@ filename = "20k.txt"
 
 
 def main(filename):
-	with open(filename) as words_file:
-		words = list(set(word.strip() for word in words_file))
-	while True:
-		length = input("Words:")
-		try:
-			length = int(length)
-			print(" ".join(random.choice(words) for _ in range(length)))
-		except ValueError as e:
-			print('Exiting...')
-			return
+    with open(filename) as words_file:
+        words = list(set(word.strip() for word in words_file))
+    while True:
+        length = input("Words:")
+        try:
+            length = int(length)
+            print(" ".join(random.choice(words) for _ in range(length)))
+        except ValueError as e:
+            print('Exiting...')
+            return
 
 
 if __name__ == "__main__":
-	main(filename)
+    main(filename)
