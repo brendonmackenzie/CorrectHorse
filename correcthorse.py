5de44da9b15d335dbcec62766fc0c8272fb13992
Brendon <brendon@primitiveman.co.nz>
2019-02-01 22:48:40 +1300

    Updated to Python 3; Fixed endless loop.

--- correcthorse.py
+++ correcthorse.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import random
 
 filename = "20k.txt"
 
+
 def main(filename):
 	with open(filename) as words_file:
 		words = list(set(word.strip() for word in words_file))
 	while True:
-		length = raw_input("Words:")
+		length = input("Words:")
 		try:
 			length = int(length)
 			print(" ".join(random.choice(words) for _ in range(length)))
-		except:
-			print("Please enter a valid number.")
+		except ValueError as e:
+			print('Exiting...')
+			return
 
 
 if __name__ == "__main__":
